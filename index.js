14cad8c74a162e2a076215385c1f4a54b54ec6a1
IxAntho <isabar735@icloud.com>
2024-02-12 11:31:59 -0500

    Set fonts

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+// Post request for new posts
+app.post("/submit", (req, res) => {
+  // Create a new Date object
+  const currentDate = new Date();
+
+  // Array of month names
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  // Get the month, day, and year
+  const month = monthNames[currentDate.getMonth()];
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+
+  // Format the date
+  const formattedDate = month + " " + day + ", " + year;
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
